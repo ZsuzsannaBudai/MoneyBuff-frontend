a1f566f6925f80266a37744291b0f1286b6bee75
ZsuzsannaBudai <bzse98@gmail.com>
2021-08-28 18:42:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+import {BrowserRouter} from "react-router-dom";
+
+function App() {
+
+  return (
+      <BrowserRouter>
+
+      </BrowserRouter>
+  );
+}
+
+export default App;
